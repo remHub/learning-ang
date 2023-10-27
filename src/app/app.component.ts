e24ecf465192f0dada895ba6b3c37fb7be5ba509
remington <remcommerce@2k02.net>
2023-10-27 15:11:43 -0700

    Two way data binding with ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Projects and Tutorials';
+
+  firstName: string = "";
+  lastName: string = "";
+  state: string  = "";
+
 }
