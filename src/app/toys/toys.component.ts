61381511f9f55dc57360986c5a13770b0f927d1a
remington <remcommerce@2k02.net>
2023-11-03 13:06:25 -0700

    Angular v16 ngTemplate directive example

--- src/app/toys/toys.component.ts
+++ src/app/toys/toys.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ToysComponent {
 
+  btnStylesAdd: string = `bg-blue-600 text-white font-thin p-1 outline outline-1 outline-black ml-2 rounded-md m-2 hover:bg-blue-300 hover:text-black hover:font-semibold`;
+
+  btnStylesDlt: string = `bg-red-600 text-white font-thin p-1 outline outline-1 outline-black ml-2 rounded-md hover:bg-red-300 hover:text-black hover:font-semibold`;
+
   simpleArray: {id: number, data: string}[] = [
     {
       id: 1,
