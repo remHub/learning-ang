3491c937cc98ef7e680ae68aeec473ca09a407d3
remington <remcommerce@2k02.net>
2023-11-03 21:24:53 -0700

    Angular v16 ngStyle directive

---
+++ src/app/style-directive/style-directive.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-style-directive',
+  templateUrl: './style-directive.component.html',
+  styleUrls: ['./style-directive.component.css']
+})
+export class StyleDirectiveComponent {
+
+  isActive: boolean = true;
+
+}
