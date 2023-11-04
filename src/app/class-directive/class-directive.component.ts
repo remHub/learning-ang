6261465fbb96e699de09f30d7e090df10e36b8f7
remington <remcommerce@2k02.net>
2023-11-04 13:10:15 -0700

    added counter

--- src/app/class-directive/class-directive.component.ts
+++ src/app/class-directive/class-directive.component.ts
@@ -7,10 +7,19 @@ import { Component } from '@angular/core';
 })
 export class ClassDirectiveComponent {
 
+  counter: number = 0;
+
   ngClassStyles: string[] = [
     `m-2 px-2 py-1 font-thin bg-blue-700 text-white rounded-md outline outline-1 outline-blue-950`,
     `m-2 px-2 py-1 font-thin bg-red-700 text-white rounded-md outline outline-1 outline-red-950`,
-    `m-2 px-2 py-1 font-thin bg-green-700 text-white rounded-md outline outline-1 outline-green-950`
+    `m-2 px-2 py-1 font-thin bg-green-700 text-white rounded-md outline outline-1 outline-green-950`,
+    `m-2 px-2 py-1 font-thin bg-slate-700 text-white rounded-md outline outline-1 outline-slate-950`
   ]
+
+  onClick() {
+    this.counter = this.counter >= 3 ? 0 : this.counter + 1;
+  }
+
+
 
 }
