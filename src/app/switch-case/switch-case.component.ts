7ef783e21eabcd0f1bc49bbb349c7ed267ec1e1c
remington <remcommerce@2k02.net>
2023-11-04 18:51:18 -0700

    added button styles

--- src/app/switch-case/switch-case.component.ts
+++ src/app/switch-case/switch-case.component.ts
@@ -12,7 +12,7 @@ export class SwitchCaseComponent {
   /**
    * Styles
    */
-  btnStyles: string = `m-2 bg-blue-600 text-white py-1 px-2 font-thin rounded-md outline outline-1 outline-blue-950`
+  btnStyles: string = `m-2 bg-gradient-to-b from-blue-400 to-blue-800 text-white py-1 px-2 font-thin rounded-md outline outline-1 outline-blue-950 shadow-lg shadow-slate-500 hover:shadow-none hover:translate-y-1 duration-200 focus:bg-gradient-to-t focus:from-blue-700 focus:to-blue-900 focus:shadow-none focus:translate-y-1`
 
   onClick(status: string) {
     this.stepForm = status;
