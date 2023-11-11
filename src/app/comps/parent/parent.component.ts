bf920f271171daf327ceb7d8423a070bf1863ff5
remington <remcommerce@2k02.net>
2023-11-11 13:44:48 -0800

    comments added

--- src/app/comps/parent/parent.component.ts
+++ src/app/comps/parent/parent.component.ts
@@ -10,10 +10,23 @@ export class ParentComponent implements AfterViewInit {
 
   receiveData: string = "Placeholder";
 
+  /**
+   * ViewChild decorator will give access to the entire
+   * component template's methods and properties.
+   * 
+   * Child Component must be imported then referenced
+   * 
+   * No changes to child components are necessary
+   */
   @ViewChild(ChildComponent) childRef!: ChildComponent;
 
   constructor(private cdr: ChangeDetectorRef) { }
 
+  /**
+   * detectChanges() method is used with ngAfterViewInit() to 
+   * force change detection that will
+   * in turn update the component template.
+   */
   ngAfterViewInit(): void {
     this.receiveData = this.childRef.messagePropertyInChild;
     this.cdr.detectChanges();
