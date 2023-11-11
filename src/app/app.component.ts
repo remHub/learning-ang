ca5c84b87d78b308284fe25c94df3c5802d4153e
remington <remcommerce@2k02.net>
2023-11-10 20:41:14 -0800

    cleaned up code a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,34 +10,8 @@ export class AppComponent {
 
   keyPressed: any;
 
-  colorTheme: number = 0;
-
-  pageSetup: any = [
-    {
-      viewportContainer: `flex flex-col h-screen w-full bg-slate-400`,
-      headerWrapper: `flex-shrink-0 m-5 bg-slate-600 rounded-lg outline outline-1 shadow-lg shadow-slate-600`,
-      headerText: `my-3 text-white text-3xl font-thin text-center cursor-pointer`,
-      bodyWrapper: `flex-grow`
-    },
-    {
-      viewportContainer: `flex flex-col h-screen w-full bg-blue-300`,
-      headerWrapper: `flex-shrink-0 m-5 bg-blue-400 rounded-lg outline outline-1 shadow-lg shadow-blue-800`,
-      headerText: `my-3 text-blue-900 text-3xl font-thin text-center cursor-pointer`,
-      bodyWrapper: `flex-grow`
-    }
-  ]
-
-  changeTheme() {
-    return this.colorTheme = this.colorTheme === 0 ? 1 : 0;
-  }
-
-  // Template Variable section
   myKeyup(uservalue: any) {
-    console.log(uservalue);
     this.keyPressed = uservalue;
   }
-
-
-
 
 }
