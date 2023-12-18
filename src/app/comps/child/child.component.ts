21298fbd8873d40c43746f3b13f55f196d12cc9a
remington <remcommerce@2k02.net>
2023-12-17 20:53:47 -0800

    more comments added

--- src/app/comps/child/child.component.ts
+++ src/app/comps/child/child.component.ts
@@ -10,7 +10,7 @@ export class ChildComponent implements OnInit {
   // create new eventEmitter and @Output()
   @Output() sendEventFromChildClass = new EventEmitter<string>();
 
-  // message to send
+  // message to send via EventEmitter
   messageToParentFromChild: string =
     `This is a message being sent to the parent from the child via EventEmitter and @Ouput() decorator.`
 
@@ -21,6 +21,11 @@ export class ChildComponent implements OnInit {
    * in the component's lifecycle.
    * 
    * ngOnInit is called after component is created but before the DOM is rendered.
+   * 
+   * It's best to use an event ( OnInit, mouse click, keypress, etc )
+   * to send a message to the parent.  This way we
+   * create a scenerio where we are expecting the the message to come in.
+   * 
    */
   ngOnInit() {
     this.sendEventFromChildClass.emit(this.messageToParentFromChild);
